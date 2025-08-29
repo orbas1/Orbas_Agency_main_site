7ff75dceec94b23ecd54b034a771f2ca6f28733c
orbas1 <ironcrest96@gmail.com>
2025-08-29 03:49:12 +0100

    Update favicon and improve bug reporting

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'OrbusLanding')));
 
